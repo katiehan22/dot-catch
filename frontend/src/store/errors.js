039306782b3405d032c59f4b900b494ca48ef1df
Mitchell Jindra <mitchjindra413@gmail.com>
2022-11-14 12:09:44 -0800

    refactor for app

--- frontend/src/store/errors.js
+++ frontend/src/store/errors.js
@@ -1,8 +1,7 @@
 import { combineReducers } from 'redux';
 import { sessionErrorsReducer } from './session';
-import { tweetErrorsReducer } from './tweets';
+
 
 export default combineReducers({
-  session: sessionErrorsReducer,
-  tweets: tweetErrorsReducer
+  session: sessionErrorsReducer
 });
