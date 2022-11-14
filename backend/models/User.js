033960e318fb5024c171ff0d9881e18be61d0f96
Brendan <brendantsuda@gmail.com>
2022-11-14 11:48:08 -0800

    add to user profile schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,17 +2,53 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = Schema({
-    username: {
-        type: String,
-        required: true
+    firstName: {
+        type: String
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     hashedPassword: {
         type: String,
         required: true
+    },
+    age: {
+        type: Number
+    },
+    location: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    sexualOrientation: {
+        type: String
+    },
+    prompt1: {
+        type: Map,
+        of: String
+    },
+    prompt2: {
+        type: Map,
+        of: String
+    },
+    prompt3: {
+        type: Map,
+        of: String
+    },
+    prompt4: {
+        type: Map,
+        of: String
+    },
+    likes: {
+        type: Map,
+        of: Boolean
+    },
+    matches: {
+        type: Map,
+        of: Boolean
     }
 }, {
     timestamps: true
