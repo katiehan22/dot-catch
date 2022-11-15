14f93880ddeb1039fd26fbc695d982cb7cb7041d
Brendan <brendantsuda@gmail.com>
2022-11-15 14:53:21 -0800

    filter deck of users to swipe thru

--- frontend/src/components/MainPage/SwipeView/SwipeCards/SwipeCards.js
+++ frontend/src/components/MainPage/SwipeView/SwipeCards/SwipeCards.js
@@ -6,9 +6,11 @@ import './SwipeCards.css';
 const SwipeCards = () => {
     const dispatch = useDispatch();
     const users = useSelector(state => state.entities.users ? Object.values(state.entities.users) : []);
-    // const userLikes = useSelector(state => state.entities.users.likes !== {} ? Object.keys(state.entities.users.likes) : []);
-    // console.log(userLikes)
+    const userLikes = useSelector(state => state.session.user.likes !== {} ? Object.keys(state.session.user.likes) : []);
+    const userMatches = useSelector(state => state.session.user.matches !== {} ? Object.keys(state.session.user.matches) : []);
     const currentUser = useSelector(state => state.session.user ? state.session.user : null);
+
+    const usersToSwipe = users.filter(user => !userLikes.includes(user._id) && user._id !== currentUser._id && !userMatches.includes(user._id));
  
     const swiped = (dir, likedUserId) => {
         if (dir === 'right') {
@@ -20,25 +22,24 @@ const SwipeCards = () => {
     return (
         <div className='swipe-cards'>
             <div className='swipe-cards-container'>
-                {users.map(user => {
-                    if (user._id !== currentUser._id) return (
-                        <TinderCard 
-                            className='swipe'
-                            key={user._id}
-                            preventSwipe={['up', 'down']}
-                            onSwipe={dir => swiped(dir, user._id)}
-                            // onCardLeftScreen={() => outOfFrame(user._id)}
-                        >
-                            <div className='card'>
-                                <div className='user-img'>
-                                    <h3>{user._id}</h3>
-                                </div>
-                                <div className='user-prof'>
-                                    <h3>bio</h3>
-                                </div>
+                {usersToSwipe.map(user => 
+                    <TinderCard 
+                        className='swipe'
+                        key={user._id}
+                        preventSwipe={['up', 'down']}
+                        onSwipe={dir => swiped(dir, user._id)}
+                        // onCardLeftScreen={() => outOfFrame(user._id)}
+                    >
+                        <div className='card'>
+                            <div className='user-img'>
+                                <h3>{user._id}</h3>
                             </div>
-                        </TinderCard>
-                )})}
+                            <div className='user-prof'>
+                                <h3>bio</h3>
+                            </div>
+                        </div>
+                    </TinderCard>
+                )}
             </div>
         </div>
     )
