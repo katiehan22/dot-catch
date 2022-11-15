322a5f8e9ca059e30f75f42560aa9bbde65559e1
Brendan <brendantsuda@gmail.com>
2022-11-15 12:17:57 -0800

    start frontend user likes

--- frontend/src/components/MainPage/SwipeView/SwipeCards/SwipeCards.js
+++ frontend/src/components/MainPage/SwipeView/SwipeCards/SwipeCards.js
@@ -4,31 +4,35 @@ import './SwipeCards.css';
 
 const SwipeCards = () => {
     const users = useSelector(state => state.users ? Object.values(state.users) : []);
-
-    const swiped = (dir, nameToDelete) => console.log("removing: " + nameToDelete);
-    const outOfFrame = nameLeft => console.log(nameLeft + " left the screen!")
+    const currentUserId = useSelector(state => state.session.user ? state.session.user._id : null);
+ 
+    const swiped = (dir, likedUserId) => {
+        console.log("removing: " + likedUserId);
+    }
+    const outOfFrame = nameLeft => console.log(nameLeft + " left the screen!");
 
     return (
         <div className='swipe-cards'>
             <div className='swipe-cards-container'>
-                {users.map(user => (
-                    <TinderCard 
-                        className='swipe'
-                        key={user._id}
-                        preventSwipe={['up', 'down']}
-                        onSwipe={dir => swiped(dir, user._id)}
-                        onCardLeftScreen={() => outOfFrame(user._id)}
-                    >
-                        <div className='card'>
-                            <div className='user-img'>
-                                <h3>{user._id}</h3>
+                {users.map(user => {
+                    if (user._id !== currentUserId) return (
+                        <TinderCard 
+                            className='swipe'
+                            key={user._id}
+                            preventSwipe={['up', 'down']}
+                            onSwipe={dir => swiped(dir, user._id)}
+                            onCardLeftScreen={() => outOfFrame(user._id)}
+                        >
+                            <div className='card'>
+                                <div className='user-img'>
+                                    <h3>{user._id}</h3>
+                                </div>
+                                <div className='user-prof'>
+                                    <h3>bio</h3>
+                                </div>
                             </div>
-                            <div className='user-prof'>
-                                <h3>bio</h3>
-                            </div>
-                        </div>
-                    </TinderCard>
-                ))}
+                        </TinderCard>
+                )})}
             </div>
         </div>
     )
