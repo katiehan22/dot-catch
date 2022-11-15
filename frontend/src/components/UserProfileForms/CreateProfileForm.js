184bf3cc4ccfe9f8df4e4c9931747d9242bd6f1f
katiehan22 <katiehan22@gmail.com>
2022-11-15 13:57:11 -0800

    show errors

--- frontend/src/components/UserProfileForms/CreateProfileForm.js
+++ frontend/src/components/UserProfileForms/CreateProfileForm.js
@@ -46,66 +46,68 @@ const CreateProfileForm = () => {
 
   return (
     <>
-      <div className="create-user-profile-container">
+      <div className="create-user-profile-page">
         <h1 className="profile-form-heading">Create your profile</h1>
-        <form className="user-profile-form">
-          <div className="user-profile-form-left">
-            <div className="first-name-container">
-              <label htmlFor="first-name">First Name: </label>
-              <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required className="profile-form-input"/>
+        <form className="user-profile-form" onSubmit={handleProfileSubmit}>
+          <div className="user-profile-form-container">
+            <div className="user-profile-form-left">
+              <div className="first-name-container">
+                <label htmlFor="first-name">First Name: </label>
+                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required className="profile-form-input"/>
+              </div>
+              <div className="age-container">
+                <label htmlFor="age">Age: </label>
+                <input type="text" value={age} onChange={(e) => setAge(e.target.value)} required className="profile-form-input" />
+              </div>
+              <div className="city-container">
+                <label htmlFor="location">City: </label>
+                <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} required className="profile-form-input" />
+              </div>
+              <div className="gender-container">
+                <label htmlFor="gender">Gender: </label>
+                <br />
+                <input type="radio" name="gender" id="gender" value="woman" onChange={(e) => setGender(e.target.value)} required />Woman
+                <input type="radio" name="gender" id="gender" value="man" onChange={(e) => setGender(e.target.value)} required />Man
+                <input type="radio" name="gender" id="gender" value="nonbinary" onChange={(e) => setGender(e.target.value)} required />Nonbinary
+              </div>
+              <label htmlFor="gender-preference">I'm Looking For: </label>
+              <input type="radio" name="gender-preference" id="gender-preference" value="men" onChange={(e) => setGenderPreference(e.target.value)} required />Men
+              <input type="radio" name="gender-preference" id="gender-preference" value="women" onChange={(e) => setGenderPreference(e.target.value)} required />Women
+              <input type="radio" name="gender-preference" id="gender-preference" value="nonbinary" onChange={(e) => setGenderPreference(e.target.value)} required />Nonbinary people
+              <input type="radio" name="gender-preference" id="gender-preference" value="no-preference" onChange={(e) => setGenderPreference(e.target.value)} required />No preference
             </div>
-            <div className="age-container">
-              <label htmlFor="age">Age: </label>
-              <input type="text" value={age} onChange={(e) => setAge(e.target.value)} required className="profile-form-input" />
-            </div>
-            <div className="city-container">
-              <label htmlFor="location">City: </label>
-              <input type="text" value={location} onChange={(e) => setLocation(e.target.value)} required className="profile-form-input" />
-            </div>
-            <div className="gender-container">
-              <label htmlFor="gender">Gender: </label>
+            <div className="user-profile-form-right">  
+              <label htmlFor="language">Favorite Programming Language: </label>
+              <select name="language" value={favLang} onChange={(e) => setFavLang(e.target.value)}>
+                <option value="" disabled>Select Favorite Language</option>
+                <option value="javascript">Javascript</option>
+                <option value="python">Python</option>
+                <option value="c">C</option>
+                <option value="ruby">Ruby</option>
+                <option value="java">Java</option>
+                <option value="html-css">HTML/CSS</option>
+                <option value="sql">SQL</option>
+              </select>
               <br />
-              <input type="radio" name="gender" id="gender" value="woman" onChange={(e) => setGender(e.target.value)} required />Woman
-              <input type="radio" name="gender" id="gender" value="man" onChange={(e) => setGender(e.target.value)} required />Man
-              <input type="radio" name="gender" id="gender" value="nonbinary" onChange={(e) => setGender(e.target.value)} required />Nonbinary
+              <label htmlFor="tabs-spaces">Tabs vs. Spaces: </label>
+              <input type="radio" name="tabs-spaces" id="tabs-spaces" value="tabs" onChange={(e) => setTabSpace(e.target.value)} required />Tabs
+              <input type="radio" name="tabs-spaces" id="tabs-spaces" value="spaces" onChange={(e) => setTabSpace(e.target.value)} required />Spaces
+              <br />
+              <label htmlFor="mac-pc">Mac vs. PC: </label>
+              <input type="radio" name="mac-pc" id="mac-pc" value="mac" onChange={(e) => setMacPc(e.target.value)} required />Mac
+              <input type="radio" name="mac-pc" id="mac-pc" value="pc" onChange={(e) => setMacPc(e.target.value)} required />PC
+              <br />
+              <label htmlFor="light-dark">Light Theme vs. Dark Theme: </label>
+              <input type="radio" name="light-dark" id="light-dark" value="light" onChange={(e) => setLightDark(e.target.value)} required />Light
+              <input type="radio" name="light-dark" id="light-dark" value="dark" onChange={(e) => setLightDark(e.target.value)} required />Dark
+              <br />
+              <label htmlFor="bio">Bio: </label>
+              <textarea value={bio} onChange={(e) => setBio(e.target.value)} required placeholder="What else do you want others to know about you?"/>
             </div>
-            <label htmlFor="gender-preference">I'm Looking For: </label>
-            <input type="radio" name="gender-preference" id="gender-preference" value="men" onChange={(e) => setGenderPreference(e.target.value)} required />Men
-            <input type="radio" name="gender-preference" id="gender-preference" value="women" onChange={(e) => setGenderPreference(e.target.value)} required />Women
-            <input type="radio" name="gender-preference" id="gender-preference" value="nonbinary" onChange={(e) => setGenderPreference(e.target.value)} required />Nonbinary people
-            <input type="radio" name="gender-preference" id="gender-preference" value="no-preference" onChange={(e) => setGenderPreference(e.target.value)} required />No preference
           </div>
-          <div className="user-profile-form-right">  
-            <label htmlFor="language">Favorite Programming Language: </label>
-            <select name="language" value={favLang} onChange={(e) => setFavLang(e.target.value)}>
-              <option value="" disabled>Select Favorite Language</option>
-              <option value="javascript">Javascript</option>
-              <option value="python">Python</option>
-              <option value="c">C</option>
-              <option value="ruby">Ruby</option>
-              <option value="java">Java</option>
-              <option value="html-css">HTML/CSS</option>
-              <option value="sql">SQL</option>
-            </select>
-            <br />
-            <label htmlFor="tabs-spaces">Tabs vs. Spaces: </label>
-            <input type="radio" name="tabs-spaces" id="tabs-spaces" value="tabs" onChange={(e) => setTabSpace(e.target.value)} required />Tabs
-            <input type="radio" name="tabs-spaces" id="tabs-spaces" value="spaces" onChange={(e) => setTabSpace(e.target.value)} required />Spaces
-            <br />
-            <label htmlFor="mac-pc">Mac vs. PC: </label>
-            <input type="radio" name="mac-pc" id="mac-pc" value="mac" onChange={(e) => setMacPc(e.target.value)} required />Mac
-            <input type="radio" name="mac-pc" id="mac-pc" value="pc" onChange={(e) => setMacPc(e.target.value)} required />PC
-            <br />
-            <label htmlFor="light-dark">Light Theme vs. Dark Theme: </label>
-            <input type="radio" name="light-dark" id="light-dark" value="light" onChange={(e) => setLightDark(e.target.value)} required />Light
-            <input type="radio" name="light-dark" id="light-dark" value="dark" onChange={(e) => setLightDark(e.target.value)} required />Dark
-            <br />
-            <label htmlFor="bio">Bio: </label>
-            <textarea value={bio} onChange={(e) => setBio(e.target.value)} required placeholder="What else do you want others to know about you?"/>
-          </div>
+          <input type="submit" value="Create Profile" className="create-profile-button" />
         </form>
-        <button className="create-profile-button" onClick={handleProfileSubmit}>Create Profile</button>
-        <div className="errors">{errors?.users.age}</div>
+        {/* <div className="errors">{errors?.users}</div> */}
       </div>
     </>
   )
