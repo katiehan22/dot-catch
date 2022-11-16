bafeb902bff8887bf9cbf234f3352d1124476833
katiehan22 <katiehan22@gmail.com>
2022-11-15 17:57:03 -0800

    start aws setup

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
     secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
-    isProduction: process.env.NODE_ENV === 'production'
+    isProduction: process.env.NODE_ENV === 'production',
+    s3AccessKey: process.env.S3_ACCESS_KEY,
+    s3AccessSecret: process.env.S3_ACCESS_SECRET
 }
