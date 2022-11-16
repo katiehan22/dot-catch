61aaae0ae21f0b5500a9ed5ae69ed70782f2c7d6
Brendan <brendantsuda@gmail.com>
2022-11-16 10:26:46 -0800

    establish state with link

--- frontend/src/components/UserProfilePage/UserProfilePage.js
+++ frontend/src/components/UserProfilePage/UserProfilePage.js
@@ -1,9 +1,13 @@
+import { useLocation, Redirect } from 'react-router-dom';
 import MatchesSidebar from '../MatchesSidebar/MatchesSidebar';
 import ProfileComponent from './ProfileComponent/ProfileComponent';
 import './UserProfilePage.css';
 
 const UserProfilePage = () => {
-    
+    const location = useLocation();
+    const { user } = location.state || {};
+
+    if (!location.state?.fromApp) return <Redirect to='/main' />
 
     return (
         <section className='main-page'>
