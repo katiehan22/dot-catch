9ca6ff7459cfacd2a00757c27c470c7273541fce
Mitchell Jindra <mitchjindra413@gmail.com>
2022-11-15 19:08:39 -0800

    splash swipe

--- frontend/src/components/MainPage/MainPage.js
+++ frontend/src/components/MainPage/MainPage.js
@@ -1,9 +1,10 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SwipeView from './SwipeView/SwipeView';
 import { fetchUsers } from '../../store/users';
 import './MainPage.css';
 import { useEffect } from 'react';
 import MatchesSidebar from '../MatchesSidebar/MatchesSidebar';
+import SplashPage from '../SplashPage/SplashPage';
 
 const MainPage = () => {
     const dispatch = useDispatch();
@@ -11,10 +12,25 @@ const MainPage = () => {
         dispatch(fetchUsers());
     }, [dispatch])
 
-    return (
+    const user = useSelector(state => state.session.user)
+
+    const whichContent = () => {
+        if(user) {
+            return (
+                <>
+                    <MatchesSidebar />
+                    <SwipeView />
+                </>
+            )
+        } else {
+            return (
+                <SplashPage></SplashPage>
+            )
+        }
+    }
+    return ( 
         <>
-            <MatchesSidebar />
-            <SwipeView />
+            {whichContent()}
         </>
     )
 }
--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -1,12 +1,70 @@
-function SplashPage() {
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/mousewheel"
+import "./SplashPage.css";
+
+import { Mousewheel, Pagination } from "swiper";
+import SignupForm from "../SessionForms/SignupForm";
+import NavBar from "../NavBar/NavBar";
+
+export default function SplashPage() {
   return (
-    <>
-      <p>A Twitter Clone</p>
-      <footer>
-        Copyright &copy; 2022 Chirper
-      </footer>
-    </>
+    <div className="splash-page-container">
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={1}
+        spaceBetween={0}
+        mousewheel={{thresholdDelta: 40, sensitivity: 0.5}}
+        // pagination={{
+        //   clickable: true,
+        // }}
+        modules={[Mousewheel, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slide slide-one" style={{ backgroundColor: 'white' }}>
+            <div className="slide-text" >
+
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide-two" style={{ backgroundColor: '#77966D' }}>
+            <div className="slide-text">
+
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide-three" style={{ backgroundColor: '#EBEBEB' }}>
+            <div className="slide-text">
+
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide-four" style={{ backgroundColor: '#77966D' }}>
+            <div className="slide-text">
+
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide-five" style={{ backgroundColor: 'white' }}>
+            <div className="slide-text">
+              <h2>.start()</h2>
+            </div>
+            <div children="wrap-center">
+              <SignupForm></SignupForm>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
 
-export default SplashPage;
+
