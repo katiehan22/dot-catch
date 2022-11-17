08ff2a8516edcc5a2a523ae7a0f6ec1cc6d2940a
Mitchell Jindra <mitchjindra413@gmail.com>
2022-11-17 15:57:37 -0800

    styling

--- frontend/src/components/ListView/ListItem.js
+++ frontend/src/components/ListView/ListItem.js
@@ -1,35 +1,13 @@
 import { Link } from "react-router-dom"
-import ProfileComponent from "../UserProfilePage/ProfileComponent/ProfileComponent"
-import { useState, useEffect } from "react"
-
 
 export const ListItem = ({user}) => {
-    const [menu, setMenu] = useState(false)
-
-    const toggleView = () => {
-        if (menu) {
-            return setMenu(false)
-        } else {
-            return setMenu(true)
-        }
-    }
-
-    const closeMenu = () => {
-        setMenu(false)
-    }
-
 
     return (
-        <>
-        <section onClick={toggleView} className="profile-container">
-            <img src='https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg'></img>
-            <h3>{user.firstName}</h3>
-        </section>
-        <div className="profile-display" style={menu ? {display: 'block'} : {display: 'none'}}>
-            <ProfileComponent user={user}></ProfileComponent>
-            <button onClick={toggleView}>X</button>
-        </div>
-        </>
-
+        <Link to={{pathname: `/profile/${user._id}`, state: {fromApp: true, user: user}}}>
+            <section className="profile-container">
+                <img src={user.photos[0] ? user.photos[0] : 'https://exoffender.org/wp-content/uploads/2016/09/empty-profile.png'}></img>
+                <h3>{user.firstName}</h3>
+            </section>
+        </Link>
     )
 }
