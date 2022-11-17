76968bfd0565d029f0071f556bd7e60a8fae125d
katiehan22 <katiehan22@gmail.com>
2022-11-17 11:46:37 -0800

    Add delete profile modal

--- frontend/src/store/ui.js
+++ frontend/src/store/ui.js
@@ -1,6 +1,7 @@
 const SHOW_LOGIN_MODAL = 'ui/SHOW_LOGIN_MODAL'
 const HIDE_MODAL = 'ui/HIDE_MODAL'
 const SHOW_SIGNUP_MODAL = 'ui/SHOW_SIGNUP_MODAL'
+const SHOW_DELETE_MODAL = "ui/SHOW_DELETE_MODAL"
 
 export const showLoginModal = () => ({
     type: SHOW_LOGIN_MODAL
@@ -14,6 +15,10 @@ export const showSignupModal = () => ({
     type: SHOW_SIGNUP_MODAL
 })
 
+export const showDeleteModal = () => ({
+    type: SHOW_DELETE_MODAL
+})
+
 const uiReducer = (state = {}, action) => {
     switch(action.type){
         case SHOW_LOGIN_MODAL:
@@ -22,6 +27,8 @@ const uiReducer = (state = {}, action) => {
             return {modal: null}
         case SHOW_SIGNUP_MODAL:
             return {modal: 'signup'}
+        case SHOW_DELETE_MODAL:
+            return {modal: 'delete'}
         default:
             return state
     }
