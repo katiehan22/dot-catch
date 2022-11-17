ab68cc2da49cc9081daf477de12ce74e3399cc55
Brendan <brendantsuda@gmail.com>
2022-11-17 15:57:41 -0800

    style buttons to transition between views

--- frontend/src/components/MainPage/SwipeView/SwipeView.js
+++ frontend/src/components/MainPage/SwipeView/SwipeView.js
@@ -9,8 +9,10 @@ const SwipeView = () => {
     return (
         <section className='matching-section'>
             <div className='viewing-option-btns'>
-                <button onClick={() => setShowListView(false)}>Swipe</button>
-                <button onClick={() => setShowListView(true)}>List</button>
+                <div className='btns-div'>
+                    <button style={!showListView ? {backgroundColor: '#ebebeb'} : {}} onClick={() => setShowListView(false)}>Swipe View</button>
+                    <button style={showListView ? {backgroundColor: '#ebebeb'} : {}} onClick={() => setShowListView(true)}>List View</button>
+                </div>
             </div>
             {!showListView &&
                 <section className='swipe-view-section'>
