620735220e3cd86ccc0a4785635f86b88f7d9e78
Mitchell Jindra <mitchjindra413@gmail.com>
2022-11-19 20:50:00 -0800

    chat routes

--- backend/controllers.js/chatControllers.js
+++ backend/controllers.js/chatControllers.js
@@ -39,4 +39,23 @@ const accessChat = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { accessChat}
+const fetchChats = asyncHandler(async (req, res) => {
+    try {
+        Chat.find({ users: {$elemMatch: {$eq: req.user._id}}})
+        .populate("users")
+        .populate("latestMessage")
+            .sort({updatedAt: -1})
+        .then(async (results) => {
+            results = await User.populate(results, {
+                path: "latestMessage.user_from",
+                select: "firstName"
+            })
+            res.status(200).send(results)
+        })
+    } catch (error) {
+        res.status(400)
+        throw new Error(error.message)
+    }
+})
+
+module.exports = { accessChat, fetchChats}
--- backend/routes/api/chat.js
+++ backend/routes/api/chat.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 const { requireUser } = require('../../config/passport');
-const { accessChat } = require("../../controllers.js/chat");
+const { accessChat, fetchChats } = require("../../controllers.js/chatControllers");
 
 router.route("/").post(requireUser, accessChat)
+router.route("/").get(requireUser, fetchChats)
